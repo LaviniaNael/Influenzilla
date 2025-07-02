de554cfdd64500716e332c6b38a90631eddd442d
Lavy <laviniaalfons@gmail.com>
2025-07-02 07:14:24 +0300

    V1.0

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+function click1(){
+    alert("HELLO?")
+}
+function click2(){
+    alert("STOP!")
+}
+function click3(){
+    alert("CLICKING!")
+}
+function click4(){
+    alert("BRO STOP!!!")
+}
+function click5(){
+    alert("RUDE!")
+}
+function click6(){
+    alert("WHAT DO YOU WANT?")
+}
+function click7(){
+    alert("😒")
+}
+function click8(){
+    alert("👿")
+}
+function click9(){
+    alert("I'M LOSING MY PATIENCE NOW!")
+}
